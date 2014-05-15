51e697ce80b8f071fd5ff5efca2ef2c23783f311
Karandeep Chawla <kschawla@karandeepchawla.ibm.com>
2014-05-15 11:35:08 -0700

    added load-data-from-excel page

---
+++ footer.js
@@ -0,0 +1,1 @@
+document.write('<div id="footer"><div class="container"><p class="muted credit">Made with <span class="glyphicon glyphicon-heart"></span> by IBM</p></div></div>');
---
+++ navigation.js
@@ -0,0 +1,16 @@
+document.write('<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">');
+document.write('<div class="container-fluid">');
+document.write('<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">');
+document.write('<ul class="nav navbar-nav">');
+document.write('<li class="active"><a href="index.html">Home</a></li>');
+document.write('<li><a href="analyze-with-cognos.html">Analyze with Cognos</a></li>');
+document.write('<li><a href="#">Analyze with R</a></li>');
+document.write('<li><a href="load-data-from-excel.html">Load data from Excel</a></li>');
+document.write('<li><a href="#">Sync from Cloudant</a></li>');
+document.write('</ul>');
+document.write('<ul class="nav navbar-nav navbar-right">');
+document.write('<li><a href="http://bluforcloud.com/?r=idug">BLUForCloud.com</a></li>');
+document.write('</ul>');
+document.write('</div>');
+document.write('</div>');
+document.write('</nav>');
